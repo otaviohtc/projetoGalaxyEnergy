48e559bbae7149d500369d39d2e92cd91a54b9d9
Otavio Costa <141185373+otaviohtc@users.noreply.github.com>
2023-11-27 21:05:05 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -21,4 +21,36 @@ seletores.forEach((seletor) => {
 
 })
 
+const nameInput = document.querySelector("[data-name-input]")
+const emailInput = document.querySelector("[data-email-input]")
+const descriptionInput = document.querySelector("[data-description-input]")
+const button = document.querySelector("[data-submit-button]")
 
+button.addEventListener("click", () => {
+    const name = nameInput.value
+    const email = emailInput.value
+    const description = descriptionInput.value
+
+    if (!name) {
+        alert("Preencha o nome corretamente")
+        return
+    }
+    if (!email) {
+        alert("Preencha o email corretamente")
+        return
+    }
+    if (!description) {
+        alert("Preencha a descrição corretamente")
+        return
+    }
+    const text = encodeURIComponent(`Olá, sou ${name}, meu email é ${email}, ${description}`)
+    window.open(`https://api.whatsapp.com/send?phone=5519991796883&text=${text}`)
+})
+
+const navlinks = document.getElementById("navlinks");
+function showMenu() {
+    navlinks.classList.add("isOpen")
+}
+function hideMenu() {
+    navlinks.classList.remove("isOpen")
+}
